e3c803c26b9d31272c1453f23551ecbf790f5145
jabir <jabirjbr07>
2023-04-01 20:53:10 -0700

    2

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css"
+
+
+const Header=()=>{
+    return(
+        <div className="header">
+            <h1 className="title">
+            Book Hotels & Homestays
+            </h1>
+            <p className="headerDisc">
+                
+            </p>
+        
+        </div>
+    )
+}
+
+export default Header;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import "./naavbar.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBed, faBus, faCar, faPlane, faTrain } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar=()=>{
     return(
@@ -7,6 +9,29 @@ const Navbar=()=>{
                 <div className="navLogo">
                     OYO
                 </div>
+                <div className="headerList">
+                <div className="headerListItem active">
+                <FontAwesomeIcon icon={faBed} />
+                <span className="icon">Rooms</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span className="icon">Flights</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span className="icon">Car Rentals</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faTrain} />
+                <span className="icon">Trains</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faBus} />
+                <span className="icon">Bus</span>
+                </div>
+            </div>
+
                 <div className="navItems">
                     <button className="navButton">Register</button>
                     <button className="navButton">Login</button>
